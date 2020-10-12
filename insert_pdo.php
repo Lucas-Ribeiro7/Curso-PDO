d465d723de452ee9eeca8e5254c65144e57ff4cd
Lucas Ribeiro <caetanolucasribeiro@gmail.com>
2020-10-12 10:31:57 -0300

    Upload Aula 2

---
+++ insert_pdo.php
@@ -0,0 +1,23 @@
+<?php
+    //-----------------Conexão---------------
+        try {
+            $pdo = new PDO('mysql:dbname=cadastropdo;host=localhost', 'root', '');
+        } catch (\PDOException $e) {
+            echo "Erro com o Banco de Dados" . $e->getMessage();
+        }
+        catch (\Exception $e) {
+            echo "Erro Generico" . $e->getMessage();
+        }
+    //----------------Insert-----------------
+        //1° Forma
+            $res = $pdo->prepare("INSERT INTO pessoa (nome, telefone, email) VALUES (:n, :t, :e)");
+            //Substituições
+            $res->bindValue(":n", "Maria"); 
+            $res->bindValue(":t", "4");
+            $res->bindValue(":e", "teste@gamil.com");
+            $res->execute();
+
+        //2° Forma
+            $res = $pdo->query("INSERT INTO pessoa (nome, telefone, email) VALUES ('Alfa', '656', 'alfa@gmail.com')");
+
+    
